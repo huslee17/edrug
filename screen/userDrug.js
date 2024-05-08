7c6956d777ceffde070f0490a9f1ce3f1ee74d84
u <husleeh020@gmail.com>
2024-05-09 01:33:33 +0800

    h

--- screen/userDrug.js
+++ screen/userDrug.js
@@ -1,12 +1,54 @@
-import React from "react";
-import { View, Text } from "react-native";
+"use client";
+
+import React, { useState } from "react";
+import { View, SafeAreaView, StyleSheet } from "react-native";
+import { SegmentedButtons } from "react-native-paper";
 
 const UserDrugScreen = () => {
+  const [value, setValue] = useState("");
+
   return (
     <View>
-      <Text>UserDrugScreen</Text>
+      <SafeAreaView style={styles.container}>
+        <SegmentedButtons
+          value={value}
+          onValueChange={setValue}
+          buttons={[
+            {
+              value: "walk",
+              label: "Walking",
+            },
+            {
+              value: "train",
+              label: "Transit",
+            },
+            { value: "1", abel: "Driving" },
+            { value: "drive", abel: "Driving" },
+          ]}
+        />
+      </SafeAreaView>
     </View>
   );
 };
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+  input: {
+    width: "100%",
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 10,
+  },
+});
 export default UserDrugScreen;
